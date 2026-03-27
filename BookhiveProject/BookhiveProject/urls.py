095025d647c3338bbe8fd7e1f68c9138716e3967
borrahemanth <hemanthborra1222@gmail.com>
2026-03-26 19:51:04 -0500

    Hemanth: update URL routing for home, search, and book detail

--- BookhiveProject/BookhiveProject/urls.py
+++ BookhiveProject/BookhiveProject/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from myapp import views
 
 urlpatterns = [
+    path('', views.UserLogin, name='Root'),
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('register/', views.UserRegister, name='UserRegister'),
+    path('userlogin/', views.UserLogin, name='UserLogin'),
+    path('adminlogin/', views.AdminLogin, name='AdminLogin'),
+    path('home/', views.UserHome, name='UserHome'),
+    path('logout/', views.Logout, name='Logout'),
+    path('forgotpassword/', views.ForgotPassword, name='ForgotPassword'),
+    path('home/', views.UserHome, name='UserHome'),
+    path('search/', views.search_books, name='search_books'),
+    path('book/<int:id>/', views.book_detail, name='book_detail'),
 ]
