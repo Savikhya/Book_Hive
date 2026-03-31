02ba0d034aee4eeb3d11848e3f0eb090df28baa4
Savikhya <41474773+Savikhya@users.noreply.github.com>
2026-03-31 11:17:15 -0500

    Revert "Hemanth - Routing and logout redirect setup"

---
+++ BookhiveProject/myapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class UserRegistrationModel(models.Model):
+    name = models.CharField(max_length=100)
+    loginid = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=10)
+    email = models.EmailField()
+    locality = models.CharField(max_length=100, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.loginid
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
