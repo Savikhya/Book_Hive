3126ec132f590be7596e90aab6fce41f8409813e
Hemesh24 <hemesh.bavirisetti2468@gmail.com>
2026-04-18 19:42:34 -0500

    Updated admin panel and book detail template

--- BookhiveProject/myapp/admin.py
+++ BookhiveProject/myapp/admin.py
@@ -5,18 +5,21 @@ admin.site.site_header = 'Bookhive Administration'
 admin.site.site_title = 'Bookhive Admin'
 admin.site.index_title = 'Bookhive Administration'
 
+
 @admin.register(UserRegistrationModel)
 class UserRegistrationModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'loginid', 'email', 'mobile', 'city', 'state')
     search_fields = ('name', 'loginid', 'email', 'mobile')
+
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name',)
 
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'genre')
+    list_display = ('title', 'author', 'genre', 'book_pdf')
     search_fields = ('title', 'author', 'genre__name')
     list_filter = ('genre',)
